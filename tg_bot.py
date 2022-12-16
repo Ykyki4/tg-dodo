39bccb9cd360f5f0b9f3573ad7a968c876559401
Ykyki4 <jimmypantalones@gmail.com>
2022-12-16 14:41:44 +0300

    Move functions from bot_helpers to tg_bot

--- tg_bot.py
+++ tg_bot.py
@@ -3,17 +3,67 @@ from textwrap import dedent
 
 from environs import Env
 from geopy import distance
+from more_itertools import chunked
+import requests
 import redis
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler, Filters, Updater, PreCheckoutQueryHandler
 
 from api import get_access_token, get_products, get_product_by_id, download_photo, add_product_to_cart, get_cart, \
     delete_from_cart, get_all_entries, add_client_entry
-from bot_helpers import get_menu, fetch_coordinates, show_cart_to_courier
 
 
 _database = None
 logger = logging.getLogger('BotLogger')
+
+
+def get_menu(products, chunk=0):
+    products_in_chunk = 8
+    keyboard = [[InlineKeyboardButton(product['name'], callback_data=product['id'])]
+                for product in list(chunked(products, products_in_chunk))[int(chunk)]]
+
+    keyboard.append([InlineKeyboardButton('⬅️', callback_data='⬅️'),
+                     InlineKeyboardButton('➡️', callback_data='➡️')])
+    keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
+
+    return InlineKeyboardMarkup(keyboard), len(list(chunked(products, products_in_chunk)))
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
+def show_cart_to_courier(update, context):
+
+    cart_response, items_response = get_cart(context.bot_data['shop_access_token'], update.effective_user.id)
+
+    cart_text = ''
+    for item in items_response:
+        cart_text += (
+            dedent(f'''
+                {item["name"]}
+                {item["description"]}
+                {item["quantity"]} пицц в корзине за {item["meta"]["display_price"]["with_tax"]["value"]["formatted"]}
+                '''))
+
+    cart_text += f'К оплате: {context.user_data["cart_price"]}р.'
+
+    context.bot.send_message(chat_id=context.user_data["closest_pizzeria"]['courierid'],
+                             text=cart_text,)
 
 
 def start(update, context):
